f4e4b4b71170e534859ac69ec9da32b3481f7268
solangeihirwe03 <solangeduhimbazeihirwe@gmail.com>
2024-03-20 11:37:20 +0200

    new changes added

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,17 @@ module.exports = {
   theme: {
     extend: {},
   },
+  screen:{
+    'sm': {'max': '639px'},
+
+    'md': {'max': '768px'},
+
+    'lg': {'max': '1023px'},
+    
+    'xl': {'max': '1279px'},
+
+    '2xl': {'max': '1535px'}
+  },
   plugins: [],
 
 }
